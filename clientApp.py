6216d86078e4adef59931dc0b30a8b54c4932bde
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2021-07-27 10:25:22 +0530

    Update clientApp.py

--- clientApp.py
+++ clientApp.py
@@ -24,8 +24,6 @@ def predictRoute():
     result = speechToText.speech2Text("audio123.wav")
     return jsonify({"Result" : str(result)})
 
-
-
 #port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=port)
